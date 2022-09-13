a3c654e04be2601d4a34745e4d4d7756979da461
Thyrus Gorges <me@thyrusgorges.com>
2022-09-13 16:49:40 -0700

    Move wrapper.rb to lib directory

--- wrapper.rb
+++
@@ -1,8 +0,0 @@
-class DisplayWrapper
-  OSASCRIPT_BIN = `which osascript`.chomp
-
-  def self.notify(text:, title:, subtitle:, sound: "Frog")
-    command = %(#{OSASCRIPT_BIN} -e 'display notification "#{text}" with title "#{title}" subtitle "#{subtitle}" sound name "#{sound}"')
-    system(command)
-  end
-end
